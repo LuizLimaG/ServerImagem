c6bc4aafb0bf483d134fabb462125dcb6ed0a4af
LUIZ_FELIPE_LIMA_GUIMARAES <72401214@SIGMAB29.BARROCA>
2024-09-12 11:21:38 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('profileImage'), (req, res) => {
+  try {
+    res.status(200).json({
+      message: 'Imagem enviada com sucesso!',
+      file: req.file,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao enviar a imagem.', error });
+  }
+});
+
+app.use('/uploads', express.static('uploads'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
